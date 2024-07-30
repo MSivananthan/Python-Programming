e2a17af90229860bb941cfffe992f6a51699f4d0
M Sivananthan <113927784+MSivananthan@users.noreply.github.com>
2024-07-30 22:45:30 +0530

    Number is even or odd

---
+++ Num_odd_even.py
@@ -0,0 +1,7 @@
+#taking input from the user
+num = int(input("Enter a number to check even/odd "))  
+#if number is divisible by 2
+if num%2 == 0:
+   print(num,"is even number")
+else:
+   print(num,"is odd number")
