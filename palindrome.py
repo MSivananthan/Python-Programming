5f781629b0c19b75bfe0fe89e8d0ea54c9f91456
M Sivananthan <113927784+MSivananthan@users.noreply.github.com>
2024-07-30 22:35:14 +0530

    Palindrome number using iterative method

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+n = int(input("please give a number : "))
+reverse,temp = 0,n
+while temp!=0:
+    reverse = reverse*10 + temp%10;        
+    temp=temp//10;
+if reverse==n:
+    print("number is palindrom")
+else:
+    print("number is not palindrom")
